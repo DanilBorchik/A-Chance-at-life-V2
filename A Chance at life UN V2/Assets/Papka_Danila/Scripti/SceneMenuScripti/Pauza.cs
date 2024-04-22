27dd694a7cc314b484faae6f37cba9b3e4b2da37
DanilBorchik <154080463+DanilBorchik@users.noreply.github.com>
2024-04-22 17:57:18 +0300

    Визуал + пауза
    Добавил пак с нормальной травой + добавил паузу что делали в воскресенье (она пока странно работает)

---
+++ A Chance at life UN V2/Assets/Papka_Danila/Scripti/SceneMenuScripti/Pauza.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Pauza : MonoBehaviour
+{
+    [SerializeField] GameObject _pausePanel;
+
+    bool _isPaused = false;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (_isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+    public void PauseGame()
+    {
+        _pausePanel.SetActive(true);
+        Time.timeScale = 0;
+        _isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        _pausePanel.SetActive(false);
+        Time.timeScale = 1;
+        _isPaused = false;
+    }
+
+    public void MainMenuButton()
+    {
+        Time.timeScale = 1;
+        _isPaused = false;
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+}
