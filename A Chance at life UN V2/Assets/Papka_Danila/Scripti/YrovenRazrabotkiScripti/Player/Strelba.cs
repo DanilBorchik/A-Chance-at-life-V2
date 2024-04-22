477b4c64c1eb8210bf29fb2e139f47282220c8b7
DanilBorchik <154080463+DanilBorchik@users.noreply.github.com>
2024-04-22 21:40:40 +0300

    Механика стрельбы

---
+++ A Chance at life UN V2/Assets/Papka_Danila/Scripti/YrovenRazrabotkiScripti/Player/Strelba.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Strelba : MonoBehaviour
+{
+    public Pyla PylaPrefab;
+    public PeredvizhenieIgroka _PeredvizhenieIgroka;
+    float timer = 0f;
+    public float timerDlaPubliki = 0.8f;
+    public float CostManaF = 10;
+
+    //public AudioSource Fireball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (timer >= timerDlaPubliki)
+            {
+                //if (_PeredvizhenieIgroka._mana >= CostManaF)
+                //{
+                    Instantiate(PylaPrefab, transform.position, transform.rotation);
+                    timer = 0;
+                    //Fireball.Play();
+                //}
+            }
+        }
+    }
+}
