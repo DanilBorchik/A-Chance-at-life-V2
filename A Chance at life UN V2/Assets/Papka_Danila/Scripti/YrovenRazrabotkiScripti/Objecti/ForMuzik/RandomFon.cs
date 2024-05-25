d7d859ea519fb771d7bdfcf7211a2fb28592cadc
DanilBorchik <154080463+DanilBorchik@users.noreply.github.com>
2024-05-25 17:50:53 +0300

    Добавил разнообразие в фоновой музыке

---
+++ A Chance at life UN V2/Assets/Papka_Danila/Scripti/YrovenRazrabotkiScripti/Objecti/ForMuzik/RandomFon.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomFon : MonoBehaviour
+{
+    public List<AudioSource> _Fon;
+    void Start()
+    {
+        _Fon[Random.Range(0, _Fon.Count)].Play();
+    }
+}
